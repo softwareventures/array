54826ffe9c45b1d5edc4c48305c790454ae48aa6
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-03-09 08:05:53 +0000

    test(or): add or test

--- index.test.ts
+++ index.test.ts
@@ -26,6 +26,7 @@ import {
     map,
     maximum,
     minimum,
+    or,
     partition,
     partitionWhile,
     product,
@@ -238,6 +239,12 @@ test("and", t => {
     t.true(and([true, true, true]));
     t.false(and([true, false, true]));
     t.true(and([]));
+});
+
+test("or", t => {
+    t.true(or([true, false, true]));
+    t.false(or([false, false, false]));
+    t.false(or([]));
 });
 
 test("any", t => {
